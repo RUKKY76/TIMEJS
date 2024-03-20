b228f1e63919e965d723b88e88cd4a5e079e85ef
sunmonu rukayat <sunmonurukayat76@gmail.com>
2024-03-20 23:05:37 +0100

    all work updated

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+function formatNumber(number) {
+  return number < 10 ? `0${number}` : number;
+}
 
+
+  // Set the target date for the countdown
+const targetDate = new Date('2024-03-25T23:59:59');
 
 setInterval(function(){
+  const currentDate = new Date();
+  const remainingTime = targetDate - currentDate;
 
-  const date = new Date()
-  const day = date.getDate()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
+  // Calculate the remaining days, hours, minutes, and seconds
   
-  const dayElem = document.querySelector('.number-day div')
-  const hoursElem = document.querySelector('.number-hours div')
-  const minutesElem = document.querySelector('.number-minutes div')
-  const secondsElem = document.querySelector('.number-seconds div')
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ));
 
-  dayElem.innerText = day
-  hoursElem.innerText = hours
-  minutesElem.innerText = minutes
-  secondsElem.innerText = seconds
+    // const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+      // Update the countdown elements with the remaining time
+      document.querySelector('.number-day div').innerHTML = formatNumber(days);
+      document.querySelector('.number-hours div').innerHTML = formatNumber(hours);
+     
+      
+      document.querySelector('.number-minutes div').innerHTML = formatNumber(minutes);
+    document.querySelector('.number-seconds div').innerHTML = formatNumber(seconds);
+
 
 },1000) 
 
